af00253ece81df078f828f0b3f640db05487d375
Leslie Borst <leslieaborst@gmail.com>
2018-10-08 22:01:25 -0400

    lab 2 is adding functionality to lab 1

--- components/todo/todo-controller.js
+++ components/todo/todo-controller.js
@@ -5,8 +5,25 @@
             const $ctrl = this;
             $ctrl.list = [
                 {items: "Walk the dog", complete: true}, 
-                {items: "Go to the store", complete: false}, 
+                {items: "Go to the store", complete: true}, 
                 {items: "Get gas", complete: true}, 
                 {items: "Wash the Car", complete: true}];
+
+            $ctrl.addTask = function() {
+                    $ctrl.list.push({items: $ctrl.newTask, complete: true});
+                    $ctrl.newtask = "";
+                };
+
+            $ctrl.removeTask = function(index) {
+                $ctrl.list.splice(index, 1);
+            };
+
+            $ctrl.completeTask = function(todo) {
+                todo.complete = false;
+            }
+
+            $ctrl.undoTask = function(todo) {
+                todo.complete = true;
+            }
         });
 }
